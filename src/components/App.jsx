81a1edf6658157552d550535bcffd05b21499943
endryuman <132812966+endryuman@users.noreply.github.com>
2023-11-19 10:44:12 +0200

    prepare

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template333
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { nanoid } from 'nanoid';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    if (this.checkContactExists(newContact.name)) {
+      return;
+    }
+    const contactsObj = {
+      ...newContact,
+      id: nanoid(),
+    };
+    this.setState(prev => ({
+      contacts: [...prev.contacts, contactsObj],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    if (filter === '') {
+      return contacts;
+    } else {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter)
+      );
+    }
+  };
+
+  deleteContact = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(el => el.id !== id),
+    }));
+  };
+
+  checkContactExists = newName => {
+    const { contacts } = this.state;
+    const isNameDuplicate = contacts.some(contact => contact.name === newName);
+
+    if (isNameDuplicate) {
+      alert(`${newName} is already in contacts`);
+      return true;
+    }
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <div>
+        <Section title="Phonebook">
+          <ContactForm addContact={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactsList
+            contacts={visibleContacts}
+            deleteContact={this.deleteContact}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
