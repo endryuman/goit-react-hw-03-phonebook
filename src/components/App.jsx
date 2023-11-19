c87cc441ad2074b85f5b14a291f75e83ead005be
endryuman <132812966+endryuman@users.noreply.github.com>
2023-11-19 15:56:42 +0200

    done i hope

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -28,6 +23,15 @@ export class App extends Component {
       contacts: [...prev.contacts, contactsObj],
     }));
   };
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.contacts.length > 0 &&
+      prevState.contacts.length !== this.state.contacts.length
+    ) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
@@ -61,8 +65,18 @@ export class App extends Component {
     }
   };
 
+  componentDidMount() {
+    const localData = localStorage.getItem('contacts');
+    if (localData) {
+      this.setState({ contacts: JSON.parse(localData) });
+    } else
+      this.setState({
+        contacts: [{ id: 'id-1', name: 'Contact Number', number: '459-12-56' }],
+      });
+  }
+
   render() {
-    const { filter } = this.state;
+    const { filter, contacts } = this.state;
     const visibleContacts = this.getVisibleContacts();
     return (
       <div>
